8489bf76d849dc6bccf5b803a2f0d6537af802a1
seirousAlpha <huangyong@iscas.ac.cn>
2016-08-31 08:56:54 +0800

    8.31 v0.0.15 test after install success3

--- testcode.py
+++ testcode.py
@@ -3,7 +3,7 @@ import os
 from pyblade import scan
 
 dir = os.path.abspath('.')
-file = os.path.join(dir, 'tests\\sample2.py')
+file = os.path.join(dir, 'tests/sample2.py')
 fd = open(file, 'r+')
 strings = fd.read()
 
